588669bc9579a67adf59592d42a41263f3ec71bd
Okpara007 <chinemeremokpara93@gmail.com>
2025-02-01 18:07:24 +0100

    security update

--- stage0/settings.py
+++ stage0/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^$gz6akb*9n8qaoo)@qcp%1=8ag_h6z)5&q)ejhn!s74mn2)5='
+SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-^$gz6akb*9n8qaoo)@qcp%1=8ag_h6z)5&q)ejhn!s74mn2)5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
