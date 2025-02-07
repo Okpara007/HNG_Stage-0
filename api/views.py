4b51f99bced21211b3629c5940da44d6af92d0e9
Okpara007 <chinemeremokpara93@gmail.com>
2025-02-07 10:09:54 +0100

    changed github url

--- api/views.py
+++ api/views.py
@@ -5,6 +5,6 @@ def get_info(request):
     data = {
         "email": "chinemeremokpara93@gmail.com",  
         "current_datetime": datetime.utcnow().isoformat() + "Z", 
-        "github_url": "https://github.com/Okpara007/HNG_Stage-0.git"  
+        "github_url": "https://github.com/Okpara007/HNG_Stage-0"  
     }
     return JsonResponse(data)
