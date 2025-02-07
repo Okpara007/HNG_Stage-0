de693e1ee14c3bca9bb677909109c05159f60f1e
Okpara007 <chinemeremokpara93@gmail.com>
2025-02-07 10:31:54 +0100

    modified git url

--- stage0/views.py
+++ stage0/views.py
@@ -5,6 +5,6 @@ def homepage(request):
     data = {
         "email": "chinemeremokpara93@gmail.com",  
         "current_datetime": datetime.utcnow().isoformat() + "Z", 
-        "github_url": "https://github.com/Okpara007/HNG_Stage-0.git"  
+        "github_url": "https://github.com/Okpara007/HNG_Stage-0"  
     }
     return JsonResponse(data)  # Return the JSON directly when accessing the root URL
